3d7d5e4fafb3b94d12364ae43870795cbab06852
Chief Nelson <chief@Chiefs-MacBook-Pro.local>
2019-11-27 10:56:39 -0500

    Made player movement progress

--- Tutorial Project/Assets/Scripts/PlayerMovement.cs
+++ Tutorial Project/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Rigidbody rb;
+
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
+        //make cursor invisible
+        Cursor.lockState = CursorLockMode.Locked;
+
+        //keep the player from falling over
+        rb.freezeRotation = false;
     }
 
     // FixedUpdate is called once per fixed length of time (use for physics)
     void FixedUpdate()
     {
-        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = transform.right * -speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = transform.right * speed;
+        }
     }
 }
