511e0034f94d9db99cc6596ccb0abce5802cc237
Chief Nelson <chief@Chiefs-MacBook-Pro.local>
2020-01-14 09:56:34 -0500

    Added player jumping
    Double jump glitch present

--- Tutorial Project/Assets/Scripts/PlayerMovement.cs
+++ Tutorial Project/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
+    public float jumpMultiplier;
+
+    private bool isJumping;
+    private float timeInAir;
+
+    [SerializeField] private AnimationCurve jumpFalloff;
 
     CharacterController cc;
 
@@ -17,12 +23,40 @@ public class PlayerMovement : MonoBehaviour
     // FixedUpdate is called once per fixed length of time (use for physics)
     void FixedUpdate()
     {
+        //get raw data
         float vertInput = Input.GetAxis("Vertical");
         float horzInput = Input.GetAxis("Horizontal");
 
+        //move player in each direction
         Vector3 forwardMove = transform.forward * vertInput;
         Vector3 strafeMove = transform.right * horzInput;
 
         cc.SimpleMove((forwardMove + strafeMove) * speed);
+
+        jumpInput();
+    }
+
+
+    void jumpInput()
+    {
+        //check for player input
+        isJumping = Input.GetAxis("Jump") != 0 && !isJumping && cc.isGrounded;
+        
+        //evaluate upward movement based of jump curve
+        if (isJumping)
+        {
+            float jumpForce = jumpMultiplier * jumpFalloff.Evaluate(timeInAir);
+            
+            cc.Move(Vector3.up * jumpForce);
+
+            timeInAir += Time.deltaTime;
+        }
+
+        //check for upward obstruction or a floor below
+        if (cc.isGrounded || cc.collisionFlags == CollisionFlags.Above)
+        {
+            isJumping = false;
+            timeInAir = 0;
+        }
     }
 }
