2a6b34005659477eede861353519e38e17df2d51
Chief Nelson <chief@Chiefs-MacBook-Pro.local>
2020-01-19 16:51:21 -0500

    Animates player walking
    Animation doesn't loop, this will be addressed

--- Tutorial Project/Assets/Scripts/PlayerAnimation.cs
+++ Tutorial Project/Assets/Scripts/PlayerAnimation.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerAnimation : MonoBehaviour
 {
+    GameManager gm;
+
+    Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gm = GameManager.getInstance();
+
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        anim.SetBool("movement", gm.getPlayerMoveSpeed() != 0);
     }
 }
