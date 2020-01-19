5d84d6b85554c3d09e9cd0602bb0b0fb43e7c4af
Chief Nelson <chief@Chiefs-MacBook-Pro.local>
2020-01-19 16:11:53 -0500

    Added singleton GameManager

---
+++ Tutorial Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // singleton
+    private static GameManager g;
+
+    //[SerializeField] private GameObject player;
+
+
+    // hide the constructor
+    private GameManager()
+    {
+
+    }
+
+    // get the instance of the singleton
+    public static GameManager getInstance()
+    {
+        return g;
+    }
+
+    // Awake is called before the first frame update
+    private void Awake()
+    {
+        if (g == null)
+        {
+            g = this;
+        }
+    }
+}
