37134123a0cc13811df1a62bfaef7282c8781e0c
Chief Nelson <chief@Chiefs-MacBook-Pro.local>
2020-02-11 17:15:22 -0500

    Added player behavior class

--- Tutorial Project/Assets/Scripts/PlayerAnimation.cs
+++ Tutorial Project/Assets/Scripts/PlayerAnimation.cs
@@ -13,7 +13,6 @@ public class PlayerAnimation : MonoBehaviour
 
     private Animator anim;
 
-    private AnimationClip currentClip;
 
     // Start is called before the first frame update
     void Start()
@@ -34,12 +33,11 @@ public class PlayerAnimation : MonoBehaviour
         anim.SetBool("jump", gm.getPlayerJumping());
         anim.SetBool("aim", gm.getPlayerAiming());
         anim.SetFloat("aim walk speed", gm.getPlayerMomentum() * aimWalkSpeedMultiplier);
+    }
 
-        // get the current state of the animator
-        if (anim.GetCurrentAnimatorClipInfo(0).Length > 0)
-        {
-            currentClip = anim.GetCurrentAnimatorClipInfo(0)[0].clip;
-            Debug.Log(currentClip.name);
-        }
+    public bool isFiring()
+    {
+        AnimatorStateInfo inf = anim.GetCurrentAnimatorStateInfo(0);
+        return inf.IsName("Shot") || inf.IsName("Aiming_Shot");
     }
 }
---
+++ Tutorial Project/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    private GameManager gm;
+
+    // Start is called before the first frame update (use for getting other objects)
+    void Start()
+    {
+        gm = GameManager.getInstance();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
