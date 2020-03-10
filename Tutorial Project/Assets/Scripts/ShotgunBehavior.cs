4b949e938ce72fdf137336a213e3f29a9070db3b
Chief Nelson <chief@Chiefs-MacBook-Pro.local>
2020-03-10 02:04:59 -0400

    Removed outdated code

--- Tutorial Project/Assets/Scripts/ShotgunBehavior.cs
+++ Tutorial Project/Assets/Scripts/ShotgunBehavior.cs
@@ -51,9 +51,6 @@ public class ShotgunBehavior : MonoBehaviour, Observer
                         break;
                 }
             }
-
-
-            fac.createBullet(transform.forward, bulletVelocity, 0.05f, 0);
         }
     }
 }
