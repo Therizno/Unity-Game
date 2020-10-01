47768c6ce81aad24489f868cd798c81b485dbc3a
Chief Nelson <chief@Chiefs-MacBook-Pro.local>
2020-10-01 02:17:41 -0400

    Added translucent black background to HUD

--- Tutorial Project/Assets/Scripts/HUDManager.cs
+++ Tutorial Project/Assets/Scripts/HUDManager.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 using TMPro;
 
-public class GUIManager : MonoBehaviour
+public class HUDManager : MonoBehaviour
 {
+    [SerializeField] private float margin;
+
+
     [SerializeField] private GameObject playerObj;
 
     [SerializeField] private GameObject magCapacityObj;
@@ -30,6 +33,21 @@ public class GUIManager : MonoBehaviour
     void Update()
     {
         magCapacity.SetText(playerBehavior.getClipAmmo() + "/");
-        reserveCapacity.SetText("" + playerBehavior.getReserveAmmo()); 
+        reserveCapacity.SetText("" + playerBehavior.getReserveAmmo());
+
+        alignBottomRightElements();
+    }
+
+    private void alignBottomRightElements()
+    {
+        Rect HUDBox = gameObject.GetComponent<RectTransform>().rect;
+
+        float leftSideX = (HUDBox.xMax - HUDBox.xMin);
+        float bottomSideY = (HUDBox.yMax - HUDBox.yMin);
+
+        //reserveAmmoObj.transform.position = new Vector3(leftSideX - margin, bottomSideY - margin, 0);
+        reserveAmmoObj.transform.position = new Vector3(HUDBox.width - margin - reserveCapacity.renderedWidth, margin, 0);
+
+        //reserveCapacity.renderedWidth is the width of the reserve ammo text box 
     }
 }
