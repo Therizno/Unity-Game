7b52556d010ea11c6c145c679ad3a6a07cf927a7
Chief Nelson <chief@Chiefs-MacBook-Pro.local>
2020-10-07 10:43:47 -0400

    Refactored mutant move code

--- Tutorial Project/Assets/Scripts/MutantBehavior.cs
+++ Tutorial Project/Assets/Scripts/MutantBehavior.cs
@@ -18,14 +18,19 @@ public class MutantBehavior : MonoBehaviour
     // FixedUpdate is called on a fixed interval
     void FixedUpdate()
     {
-        //temp demo code//
+        //move to player//
         Vector3 playerCoords = gm.getPlayerCoords();
+        moveToward(playerCoords);
+    }
 
-        Vector3 goalPosition = new Vector3(playerCoords.x, transform.position.y, playerCoords.z);
+    private void moveToward(Vector3 coords)
+    {
+        //move towards the position on the x and z axes
+        Vector3 goalPosition = new Vector3(coords.x, transform.position.y, coords.z);
         transform.position = Vector3.MoveTowards(transform.position, goalPosition, speed);
 
-        //face the player
-        transform.LookAt(playerCoords);
+        //face the position
+        transform.LookAt(coords);
 
         //correct the monster's orientation so that it doesn't rotate on the x axis
         Vector3 transformCorrection = new Vector3(0, transform.eulerAngles.y, transform.eulerAngles.z);
