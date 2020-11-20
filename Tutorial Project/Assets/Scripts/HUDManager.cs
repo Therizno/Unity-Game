fb994cd6050faf370f33319dfa0781eb44a214d4
Chief Nelson <chief@Chiefs-MacBook-Pro.local>
2020-11-20 01:21:16 -0500

    Ammo HUD resizes automatically
    Needs some fixing to make it the right size

--- Tutorial Project/Assets/Scripts/HUDManager.cs
+++ Tutorial Project/Assets/Scripts/HUDManager.cs
@@ -7,10 +7,12 @@ using TMPro;
 public class HUDManager : MonoBehaviour
 {
     [SerializeField] private float margin;
+    [SerializeField] private float spaceBetweenElements;
 
 
     [SerializeField] private GameObject playerObj;
 
+    [SerializeField] private GameObject ammoDisplayBox;
     [SerializeField] private GameObject magCapacityObj;
     [SerializeField] private GameObject reserveAmmoObj;
 
@@ -40,14 +42,24 @@ public class HUDManager : MonoBehaviour
 
     private void alignBottomRightElements()
     {
-        Rect HUDBox = gameObject.GetComponent<RectTransform>().rect;
+        Rect HUDBox = gameObject.GetComponent<RectTransform>().rect; //rect of HUD
+        Rect ammoBox = ammoDisplayBox.GetComponent<RectTransform>().rect; //rect of ammo HUD box 
 
-        float leftSideX = (HUDBox.xMax - HUDBox.xMin);
-        float bottomSideY = (HUDBox.yMax - HUDBox.yMin);
+        //position the ammo HUD box so that it has room for the HUD elements inside with some margin
+        ammoDisplayBox.transform.position = new Vector3(HUDBox.width + (ammoBox.width/2) - (magCapacity.renderedWidth + reserveCapacity.renderedWidth + spaceBetweenElements + 2*margin), ammoDisplayBox.transform.position.y, 0);
 
-        //reserveAmmoObj.transform.position = new Vector3(leftSideX - margin, bottomSideY - margin, 0);
-        reserveAmmoObj.transform.position = new Vector3(HUDBox.width - margin - reserveCapacity.renderedWidth, margin, 0);
 
-        //reserveCapacity.renderedWidth is the width of the reserve ammo text box 
+        //make the magazine ammo HUD element stick to the left side of the ammo HUD box element
+
+        float currentY = magCapacityObj.GetComponent<RectTransform>().anchoredPosition.y;
+
+        magCapacityObj.GetComponent<RectTransform>().anchoredPosition = new Vector2(margin + (magCapacity.renderedWidth/2), currentY);
+
+
+        //make the reserve ammo HUD element stick to the left side of the mag capacity HUD element
+
+        currentY = reserveAmmoObj.GetComponent<RectTransform>().anchoredPosition.y;
+
+        reserveAmmoObj.GetComponent<RectTransform>().anchoredPosition = new Vector2(margin + magCapacity.renderedWidth + spaceBetweenElements, currentY);
     }
 }
