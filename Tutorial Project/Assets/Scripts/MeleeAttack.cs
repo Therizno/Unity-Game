782007cc0230bd88065b2bc44d4391f324a31824
Chief Nelson <chief@Chiefs-MBP.lan>
2021-03-17 10:03:35 -0600

    Added melee attack cooldown
    Also tweaked mutant cc size and attack distance

--- Tutorial Project/Assets/Scripts/MeleeAttack.cs
+++ Tutorial Project/Assets/Scripts/MeleeAttack.cs
@@ -6,26 +6,32 @@ public class MeleeAttack : MonoBehaviour
 {
     [SerializeField] private int damage;
     [SerializeField] private bool plyrTeam;
+    [SerializeField] private float cooldownTime;
 
-    // Start is called before the first frame update
-    void Start()
+    private float timeSinceAttack;
+
+
+    void Awake()
     {
-    
+        timeSinceAttack = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeSinceAttack += Time.deltaTime;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         HitboxBehavior hb = other.gameObject.GetComponent<HitboxBehavior>();
 
-        if (hb != null && plyrTeam != hb.playerTeam())
+        if (hb != null && timeSinceAttack > cooldownTime && plyrTeam != hb.playerTeam())
         {
             hb.damageParent(damage);
+
+            //reset cooldown
+            timeSinceAttack = 0;
         }
     }
 }
