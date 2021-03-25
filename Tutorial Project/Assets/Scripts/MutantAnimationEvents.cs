80a76b92296fae1c96626f4cb8c4841358849479
Chief Nelson <chief@Chiefs-MBP.lan>
2021-03-24 21:16:49 -0600

    Created mutant attack animation events
    No functionality yet

---
+++ Tutorial Project/Assets/Scripts/MutantAnimationEvents.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MutantAnimationEvent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    // any of these functions can be triggered by an animation
+
+    public void startAttack()
+    {
+
+    }
+
+    public void endAttack()
+    {
+
+    }
+}
