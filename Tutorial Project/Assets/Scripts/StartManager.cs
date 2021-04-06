f08b62b5d0812ca6b32c55cc68520d0c3ded7a73
Chief Nelson <chief@Chiefs-MBP.lan>
2021-04-05 23:26:21 -0600

    Added start button functionality (not working yet)

---
+++ Tutorial Project/Assets/Scripts/StartManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartManager : MonoBehaviour
+{
+    private Button startButton;
+
+
+    /*
+    * singleton
+    */
+
+    private static StartManager g;
+
+    // hide the constructor
+    private StartManager()
+    {
+
+    }
+
+    // get the instance of the singleton
+    public static StartManager getInstance()
+    {
+        return g;
+    }
+
+    // Awake is called before start
+    private void Awake()
+    {
+        if (g == null)
+        {
+            g = this;
+        }
+    }
+
+    /*
+     * end singleton
+     */
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton = GetComponent<Button>();
+        startButton.onClick.AddListener(startGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void startGame()
+    {
+        Debug.Log("button triggered");
+        SceneManager.LoadSceneAsync("SampleScene");
+    }
+}
