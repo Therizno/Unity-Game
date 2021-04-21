c6e378e7855fab0e5b32f80252f63768b3da4a13
Chief Nelson <chief@Chiefs-MBP.lan>
2021-04-21 10:30:04 -0600

    Added monster pathfinder class
    For flowfields

---
+++ Tutorial Project/Assets/Scripts/MonsterPathfinder.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+/*
+ * The flowfield pathfinding class
+ */
+
+public class MonsterPathfinder : MonoBehaviour
+{
+    [SerializeField] GameObject testMonster;
+
+    private MutantBehavior behavior;
+
+    private NavMeshTriangulation triMesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        behavior = testMonster.GetComponent<MutantBehavior>();
+
+        triMesh = NavMesh.CalculateTriangulation();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+
+
+    void isOccupiable(float x, float z)
+    {
+
+    }
+}
